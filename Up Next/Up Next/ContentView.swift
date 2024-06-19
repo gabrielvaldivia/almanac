f3bfe8fc7db57bf256e156f1c6b01d8ba0ae5710
gabrielvaldivia <59620829+gabrielvaldivia@users.noreply.github.com>
2024-06-19 11:51:59 -0400

    Tweaked the event component to show relative date, sort properly and added styles

--- Up Next/Up Next/ContentView.swift
+++ Up Next/Up Next/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct Event: Identifiable {
     let id = UUID()
@@ -28,9 +29,21 @@ struct ContentView: View {
             ZStack(alignment: .bottom) {
                 List {
                     ForEach(events) { event in
-                        VStack(alignment: .leading) {
-                            Text(event.title)
-                            Text(event.date, style: .date)
+                        HStack(alignment: .top) {
+                            HStack {
+                                VStack (alignment: .leading) {
+                                    Text(event.title)
+                                    Text(event.date, style: .date)
+                                        .font(.subheadline)
+                                        .foregroundColor(.gray)
+                                }
+                                Spacer()
+                            }
+
+                            Text("\(event.date.relativeDate())")
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            
                         }
                         .onTapGesture {
                             self.selectedEvent = event
@@ -62,6 +75,8 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
             }
         }
+
+        // Add Event Sheet
         .sheet(isPresented: $showAddEventSheet, onDismiss: {
             newEventTitleFocus = false
         }) {
@@ -83,8 +98,17 @@ struct ContentView: View {
                     }
                 }
             }
+            .onAppear {
+                DispatchQueue.main.async {
+                    self.newEventTitleFocus = true
+                }
+            }
         }
-        .sheet(isPresented: $showEditSheet) {
+
+        // Edit Event Sheet
+        .sheet(isPresented: $showEditSheet, onDismiss: {
+            newEventTitleFocus = false
+        }) {
             NavigationView {
                 Form {
                     TextField("Edit Event Title", text: $newEventTitle)
@@ -103,6 +127,11 @@ struct ContentView: View {
                     }
                 }
             }
+            .onAppear {
+                DispatchQueue.main.async {
+                    self.newEventTitleFocus = true
+                }
+            }
         }
     }
 
@@ -113,6 +142,7 @@ struct ContentView: View {
     func addNewEvent() {
         let newEvent = Event(title: newEventTitle, date: newEventDate)
         events.append(newEvent)
+        sortEvents()
         newEventTitle = ""
         newEventDate = Date()
     }
@@ -121,10 +151,44 @@ struct ContentView: View {
         if let selectedEvent = selectedEvent, let index = events.firstIndex(where: { $0.id == selectedEvent.id }) {
             events[index].title = newEventTitle
             events[index].date = newEventDate
+            sortEvents()
+        }
+    }
+
+    func sortEvents() {
+        events.sort { $0.date < $1.date }
+    }
+}
+
+extension Date {
+    func relativeDate() -> String {
+        let calendar = Calendar.current
+        let now = Date()
+        let startOfNow = calendar.startOfDay(for: now)
+        let startOfSelf = calendar.startOfDay(for: self)
+
+        let components = calendar.dateComponents([.day], from: startOfNow, to: startOfSelf)
+        guard let dayCount = components.day else { return "Error" }
+
+        switch dayCount {
+        case 0:
+            return "Today"
+        case 1:
+            return "Tomorrow"
+        case let x where x > 1:
+            return "In \(x) days"
+        case -1:
+            return "Yesterday"
+        case let x where x < -1:
+            return "\(abs(x)) days ago"
+        default:
+            return "Error"
         }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
